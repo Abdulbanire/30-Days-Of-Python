7ee25bab3707a17752996004151b4a66afebf7a1
Abdul Banire <14banire@gmail.com>
2026-01-29 15:25:51 +0000

    'Day 2: 30 Days of python programming'

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Abdul'
+last_name = 'Banire' 
+full_name = 'Abdul Banire' 
+country = 'England' 
+city = 'Liverpool' 
+age = 22
+year = 2003
+is_married = False  
+is_true = True      
+is_light_on = True  
+
+Personal_info = {
+  'first_name': 'Abdul',
+  'last_name': 'Banire',
+  'full_name': 'Abdul Banire', 
+  'country': 'England',  
+  'city': 'Liverpool', 
+  'age': 22,
+  'year': 2003,
+  'is_married': False,   
+  'is_true': True,       
+  'is_light_on': True,   
+}
